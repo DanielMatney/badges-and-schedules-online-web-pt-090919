4fc0ad182d3a90233eaa8aef3a0463d3891c6e17
Daniel Matney <danieljmatney@gmail.com>
2019-09-17 14:32:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator(names)
   counter = 0
   badge = []
   names.each do
-    badge[counter] = "Hello, my name is #{names[counter]}"
+    badge[counter] = "Hello, my name is #{names[counter]}."
     counter += 1
   end
   badge
